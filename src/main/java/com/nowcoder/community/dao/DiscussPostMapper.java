2a65fdb3bb8dd099a09cbe514349cbb039f79861
GreatBiscuit <318215104@qq.com>
2021-03-08 19:59:25 +0800

    FirstCommit

---
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -0,0 +1,20 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DiscussPostMapper {
+
+    //userId不是0才用  用动态sql
+    List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
+
+    //@Param  别名   动态sql的条件要用这个参数且只有一个参数就必须要别名
+    int selectDiscussPostRows(@Param("userId") int userId);
+
+
+
+}
