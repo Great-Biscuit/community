3ec83561bb049f2608e892ffd557a6f2e5a84531
GreatBiscuit <318215104@qq.com>
2021-03-13 23:16:36 +0800

    Test Commit1

--- src/test/java/com/nowcoder/community/MailTest.java
+++ src/test/java/com/nowcoder/community/MailTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,9 +18,25 @@ public class MailTest {
     @Autowired
     private MailClient mailClient;
 
+    @Autowired
+    private TemplateEngine templateEngine;
+
     @Test
     public void testTextMail() {
         mailClient.sendMail("1504544146@qq.com", "From Great Bisccuit", "<!DOCTYPE html><html> <head></head><body><h3>前端技术的划分标准</h5></body><ml>");
+    }
+
+    @Test
+    public void testHtmlMail() {
+
+        Context context = new Context();
+        context.setVariable("username", "GreatBiscuit");
+
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+
+        mailClient.sendMail("318215104@qq.com", "HTML", content);
+
     }
 
 }
