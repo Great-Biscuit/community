8d9d78bcb76f194c2477689eeb3ed4f100214196
GreatBiscuit <318215104@qq.com>
2021-03-14 01:39:49 +0800

    Test Commit1

--- src/main/java/com/nowcoder/community/util/MailClient.java
+++ src/main/java/com/nowcoder/community/util/MailClient.java
@@ -21,7 +21,7 @@ public class MailClient {
     @Autowired
     private JavaMailSender mailSender;
 
-    @Value("${spring.mail.username}")
+    @Value("${spring.mail.username} ")
     private String from;
 
     public void sendMail(String to, String subject, String content) {
