a0da082e473be18f189f0922d81e880ca992b1f3
GreatBiscuit <318215104@qq.com>
2021-03-17 20:16:05 +0800

    Test Commit1

--- src/test/java/com/nowcoder/community/MapperTest.java
+++ src/test/java/com/nowcoder/community/MapperTest.java
@@ -1,8 +1,10 @@
 package com.nowcoder.community;
 
 import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.LoginTicketMapper;
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.LoginTicket;
 import com.nowcoder.community.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,15 +27,18 @@ public class MapperTest {
     @Autowired
     private DiscussPostMapper discussPostMapper;
 
+    @Autowired
+    private LoginTicketMapper loginTicketMapper;
+
     @Test
     public void testSelectUser() {
-        User user=userMapper.selectById(101);
+        User user = userMapper.selectById(101);
         System.out.println(user);
 
-        user=userMapper.selectByName("liubei");
+        user = userMapper.selectByName("liubei");
         System.out.println(user);
 
-        user=userMapper.selectByEmail("nowcoder101@sina.com");
+        user = userMapper.selectByEmail("nowcoder101@sina.com");
         System.out.println(user);
 
     }
@@ -67,13 +72,33 @@ public class MapperTest {
 
     @Test
     public void testSelectPosts() {
-        List<DiscussPost> list=discussPostMapper.selectDiscussPosts(149,0,10);
-        for (DiscussPost post:list){
+        List<DiscussPost> list = discussPostMapper.selectDiscussPosts(149, 0, 10);
+        for (DiscussPost post : list) {
             System.out.println(post);
         }
 
         int rows = discussPostMapper.selectDiscussPostRows(149);
         System.out.println(rows);
+    }
+
+    @Test
+    public void testInsertLoginTicket() {
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(101);
+        loginTicket.setTicket("abc");
+        loginTicket.setStatus(0);
+        //到期时间
+        loginTicket.setExpired(new Date(System.currentTimeMillis() + 1000 * 60 * 10));
+        loginTicketMapper.insertLoginTicket(loginTicket);
+    }
+
+    @Test
+    public void testSelectLoginTicket() {
+        LoginTicket loginTicket = loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
+        loginTicketMapper.updateStatus("abc", 1);
+        loginTicket = loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
     }
 
 }
