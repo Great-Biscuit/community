6473160bccc3778455a3be59bb954505f3d8c7f7
GreatBiscuit <318215104@qq.com>
2021-07-18 19:39:40 +0800

    feat: 整合elasticsearch

--- src/main/java/com/nowcoder/community/CommunityApplication.java
+++ src/main/java/com/nowcoder/community/CommunityApplication.java
@@ -3,11 +3,20 @@ package com.nowcoder.community;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class CommunityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CommunityApplication.class, args);
-	}
+    @PostConstruct
+    public void init() {
+        //此处是为了解决es启动时，底层的netty和redis的底层的netty启动冲突的问题。
+        //解决方式是从Netty4Utils类中的setAvailableProcessors()方法中找到的
+        System.setProperty("es.set.netty.runtime.available.processors", "false");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
 
 }
--- src/main/java/com/nowcoder/community/entity/DiscussPost.java
+++ src/main/java/com/nowcoder/community/entity/DiscussPost.java
@@ -1,17 +1,41 @@
 package com.nowcoder.community.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
 import java.util.Date;
 
+@Document(indexName = "discusspost", type = "_doc", shards = 6, replicas = 2)
 public class DiscussPost {
 
+    @Id
     private int id;
+
+    @Field(type = FieldType.Integer)
     private int userId;
+
+    //前面存储器是存储时（存储更多分词）；后面那个是搜索时(智能分词，防止过多无用词)
+    @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_smart")
     private String title;
+
+    @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_smart")
     private String content;
+
+    @Field(type = FieldType.Integer)
     private int type;
+
+    @Field(type = FieldType.Integer)
     private int status;
+
+    @Field(type = FieldType.Date)
     private Date createTime;
+
+    @Field(type = FieldType.Integer)
     private int commentCount;
+
+    @Field(type = FieldType.Double)
     private double score;
 
     public int getId() {
