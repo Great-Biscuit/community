c2f3a36921a74a24de01db418162593acc883a01
GreatBiscuit <318215104@qq.com>
2021-07-25 16:20:55 +0800

    feat: 集成Spring Security进行认证与授权

---
+++ src/main/java/com/nowcoder/community/config/SecurityConfig.java
@@ -0,0 +1,74 @@
+package com.nowcoder.community.config;
+
+import com.nowcoder.community.util.CommunityConstant;
+import com.nowcoder.community.util.CommunityUtil;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import java.io.PrintWriter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter implements CommunityConstant {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 授权
+        http.authorizeRequests()
+                .antMatchers(
+                        "/user/setting",
+                        "/user/upload",
+                        "/discuss/add",
+                        "/comment/add/**",
+                        "/letter/**",
+                        "/notice/**",
+                        "/like",
+                        "/unlike",
+                        "/follow",
+                        "/unfollow"
+                )
+                .hasAnyAuthority(
+                        AUTHORITY_USER,
+                        AUTHORITY_ADMIN,
+                        AUTHORITY_MODERATOR
+                )
+                .anyRequest().permitAll()//其他请求都允许
+                .and().csrf().disable();//关闭csrf攻击拦截
+
+        // 权限不够时的处理
+        // 没有登录
+        // 权限不足
+        http.exceptionHandling()
+                .authenticationEntryPoint((request, response, e) -> {
+                    String xRequestedWith = request.getHeader("x-requested-with");
+                    if ("XMLHttpRequest".equals(xRequestedWith)) {//JSON异步请求
+                        response.setContentType("application/plain;charset=utf-8");
+                        PrintWriter writer = response.getWriter();
+                        writer.write(CommunityUtil.getJSONString(403, "你还没有登录哦!"));
+                    } else {//不是异步
+                        response.sendRedirect(request.getContextPath() + "/login");
+                    }
+                })
+                .accessDeniedHandler((request, response, e) -> {
+                    String xRequestedWith = request.getHeader("x-requested-with");
+                    if ("XMLHttpRequest".equals(xRequestedWith)) {
+                        response.setContentType("application/plain;charset=utf-8");
+                        PrintWriter writer = response.getWriter();
+                        writer.write(CommunityUtil.getJSONString(403, "你没有访问此功能的权限!"));
+                    } else {
+                        response.sendRedirect(request.getContextPath() + "/denied");
+                    }
+                });
+
+        // Security底层默认会拦截/logout请求,进行退出处理.
+        // 覆盖它默认的逻辑,才能执行我们自己的退出代码.
+        http.logout().logoutUrl("/securitylogout");
+    }
+
+}
--- src/main/java/com/nowcoder/community/controller/LoginController.java
+++ src/main/java/com/nowcoder/community/controller/LoginController.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -151,6 +152,7 @@ public class LoginController implements CommunityConstant {
     @GetMapping("loginOut")
     public String loginOut(@CookieValue("ticket") String ticket) {
         userService.loginOut(ticket);
+        SecurityContextHolder.clearContext();
         return "redirect:/login";
     }
 
